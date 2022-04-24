23ad12d145139fab088d77b2037a07e5b3890f6d
c-ostic <60513574+c-ostic@users.noreply.github.com>
2022-04-24 19:49:26 -0400

    NFA Implementation Part 1
    Set up NFA class
    Added instance variables and methods to parse the Regex String
    Filled in parseRegex and parseUnionGroup
    Added a custom error for invalid regex strings

---
+++ src/main/java/edu/marist/costic/NFA.java
@@ -0,0 +1,90 @@
+package edu.marist.costic;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Set;
+
+class InvalidRegexException extends Exception
+{
+    public InvalidRegexException(String message)
+    {
+        super(message);
+    }
+}
+
+public class NFA {
+    // the delta function maps a state to the list of states it is connected to on a particular symbol
+    private Dictionary<StateSymbolPair, List<String>> deltaFunction;
+
+    private Set<Character> alphabet;
+    private int states;
+
+    private String regex;
+    private int currentChar;
+
+    public NFA(String regex, Set<Character> alphabet) {
+        this.alphabet = alphabet;
+        this.regex = regex;
+        currentChar = 0;
+        states = 0;
+
+        deltaFunction = new Hashtable<StateSymbolPair,List<String>>();
+
+        try {
+            parseRegex();
+        } catch (InvalidRegexException e) {
+            Utils.error("Error parsing regex: " + e.getMessage());
+        }
+    }
+
+    private void parseRegex() throws InvalidRegexException {
+        parseUnionGroup();
+    }
+
+    private int[] parseUnionGroup() throws InvalidRegexException {
+        int[] leftSide = parseConcatGroup();
+
+        if(currentChar < regex.length()) {
+            if(regex.charAt(currentChar) != '+') {
+                throw new InvalidRegexException("Invalid characters beyond regex");
+            } else {
+                // the next character is a '+' so move past it
+                currentChar++;
+                int[] rightSide = parseUnionGroup();
+
+                // get the next 2 available state counts and make them the beginning and the end of the union
+                int unionStart = states;
+                states++;
+                int unionEnd = states;
+                states++;
+
+                // add the necessary relations to the delta function
+                addToDelta(new StateSymbolPair(unionStart), leftSide[0]);
+                addToDelta(new StateSymbolPair(unionStart), rightSide[0]);
+                addToDelta(new StateSymbolPair(leftSide[1]), unionEnd);
+                addToDelta(new StateSymbolPair(rightSide[1]), unionEnd);
+
+                return new int[] {unionStart, unionEnd};
+            }
+        } else {
+            return leftSide;
+        }
+    }
+
+    private int[] parseConcatGroup() {
+        return null;
+    }
+
+    private int[] parseKleeneGroup() {
+        return null;
+    }
+
+    private void addToDelta(StateSymbolPair pair, int result) {
+
+    }
+
+    public String convertToDot(){
+        return "";
+    }
+}
