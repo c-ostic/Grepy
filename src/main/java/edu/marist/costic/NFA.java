1bf883ecab6bcbda337301ec70ab3249ef8f0769
c-ostic <60513574+c-ostic@users.noreply.github.com>
2022-04-24 21:35:10 -0400

    NFA java docs
    Added java docs to each method in NFA

--- src/main/java/edu/marist/costic/NFA.java
+++ src/main/java/edu/marist/costic/NFA.java
@@ -14,6 +14,9 @@ class InvalidRegexException extends Exception
     }
 }
 
+/**
+ * Represents an NFA.
+ */
 public class NFA {
     // the delta function maps a state to the list of states it is connected to on a particular symbol
     private Map<StateSymbolPair, List<Integer>> deltaFunction;
@@ -24,6 +27,11 @@ public class NFA {
     private String regex;
     private int currentChar;
 
+    /**
+     * Constructs an NFA object given a regex string and an alphabet to work with.
+     * @param regex the regex string.
+     * @param alphabet the alphabet to use.
+     */
     public NFA(String regex, Set<Character> alphabet) {
         this.alphabet = alphabet;
         this.regex = regex;
@@ -39,10 +47,19 @@ public class NFA {
         }
     }
 
+    /**
+     * Starts the recursive descent through parsing the regex.
+     * @throws InvalidRegexException
+     */
     private void parseRegex() throws InvalidRegexException {
         parseUnionGroup();
     }
 
+    /**
+     * Parses a group of the regex where the lowest precedence operator is union, '+'.
+     * @return the beginning and end states given by Thompson's algorithm, inside of a int array.
+     * @throws InvalidRegexException
+     */
     private int[] parseUnionGroup() throws InvalidRegexException {
         int[] leftSide = parseConcatGroup();
 
@@ -73,6 +90,11 @@ public class NFA {
         }
     }
 
+    /**
+     * Parses a group of the regex where the lowest precedence operator is concatenation.
+     * @return the beginning and end states given by Thompson's algorithm, inside of a int array.
+     * @throws InvalidRegexException
+     */
     private int[] parseConcatGroup() throws InvalidRegexException {
         int[] leftSide = parseKleeneGroup();
         if (currentChar < regex.length() && regex.charAt(currentChar) != ')' && regex.charAt(currentChar) != '+') {
@@ -88,6 +110,11 @@ public class NFA {
         }
     }
 
+    /**
+     * Parses a group of the regex where the lowest precedence operator is Kleene star, '*'.
+     * @return the beginning and end states given by Thompson's algorithm, inside of a int array.
+     * @throws InvalidRegexException
+     */
     private int[] parseKleeneGroup() throws InvalidRegexException {
         int[] symbolGroup = parseSymbolGroup();
         if (currentChar < regex.length() && regex.charAt(currentChar) == '*') {
@@ -101,6 +128,11 @@ public class NFA {
         return symbolGroup;
     }
 
+    /**
+     * Parses a group of the regex made up of either a symbol or another regex string inside of parenthesis.
+     * @return the beginning and end states given by Thompson's algorithm, inside of a int array.
+     * @throws InvalidRegexException
+     */
     private int[] parseSymbolGroup() throws InvalidRegexException {
         if (currentChar < regex.length()) {
             if (regex.charAt(currentChar) == '(') {
@@ -139,6 +171,11 @@ public class NFA {
         }
     }
 
+    /**
+     * Adds a state and symbol pair with its destination to the deltaFunction.
+     * @param pair the starting state and the symbol paired with it.
+     * @param result the destination state from this move.
+     */
     private void addToDelta(StateSymbolPair pair, int result) {
         // get the current list associated with this pair
         List<Integer> currentList = deltaFunction.get(pair);
@@ -153,6 +190,10 @@ public class NFA {
         currentList.add(result);
     }
 
+    /**
+     * Converts the NFA to dot format.
+     * @return the dot format as a string.
+     */
     public String convertToDot() {
         String dotFormat = "digraph nfa {\n";
 
